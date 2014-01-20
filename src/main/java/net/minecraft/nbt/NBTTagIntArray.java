7a392153a8b4358aade6855fae207928b6dd567c
md_5 <git@md-5.net>
2014-01-20 13:44:07 +1100

    Catch stalling on corrupted map data / NBT arrays.

--- src/main/java/net/minecraft/nbt/NBTTagIntArray.java
+++ src/main/java/net/minecraft/nbt/NBTTagIntArray.java
@@ -25,6 +25,7 @@ public final class NBTTagIntArray implements NBTList {
         private static int[] readAccounted(DataInput datainput, NBTReadLimiter nbtreadlimiter) throws IOException {
             nbtreadlimiter.accountBytes(24L);
             int i = datainput.readInt();
+            com.google.common.base.Preconditions.checkArgument( i < 1 << 24); // Spigot
 
             nbtreadlimiter.accountBytes(4L, (long) i);
             int[] aint = new int[i];
