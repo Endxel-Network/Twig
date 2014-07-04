28502b274fa86faf4ec49d106269adde32ad3fac
Jonas Konrad <me@yawk.at>
2014-07-04 23:03:13 +0200

    Make "moved wrongly" limit configurable

--- src/main/java/net/minecraft/server/network/PlayerConnection.java
+++ src/main/java/net/minecraft/server/network/PlayerConnection.java
@@ -583,7 +583,7 @@ public class PlayerConnection extends ServerCommonPacketListenerImpl implements 
                 d10 = d6 * d6 + d7 * d7 + d8 * d8;
                 boolean flag2 = false;
 
-                if (d10 > 0.0625D) {
+                if (d10 > org.spigotmc.SpigotConfig.movedWronglyThreshold) { // Spigot
                     flag2 = true;
                     PlayerConnection.LOGGER.warn("{} (vehicle of {}) moved wrongly! {}", new Object[]{entity.getName().getString(), this.player.getName().getString(), Math.sqrt(d10)});
                 }
@@ -1400,7 +1400,7 @@ public class PlayerConnection extends ServerCommonPacketListenerImpl implements 
                                 d10 = d6 * d6 + d7 * d7 + d8 * d8;
                                 boolean flag3 = false;
 
-                                if (!this.player.isChangingDimension() && d10 > 0.0625D && !this.player.isSleeping() && !this.player.isCreative() && !this.player.isSpectator()) {
+                                if (!this.player.isChangingDimension() && d10 > org.spigotmc.SpigotConfig.movedWronglyThreshold && !this.player.isSleeping() && !this.player.isCreative() && !this.player.isSpectator()) { // Spigot
                                     flag3 = true;
                                     PlayerConnection.LOGGER.warn("{} moved wrongly!", this.player.getName().getString());
                                 }
