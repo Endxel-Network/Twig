9f5610916a1a895e8532fbbfbdfeafdbc8f92589
md_5 <git@md-5.net>
2025-05-09 19:37:09 +1000

    SPIGOT-8047: Make CraftMinecartRideable constructor consistent

--- src/main/java/org/bukkit/craftbukkit/entity/CraftMinecartRideable.java
+++ src/main/java/org/bukkit/craftbukkit/entity/CraftMinecartRideable.java
@@ -1,11 +1,11 @@
 package org.bukkit.craftbukkit.entity;
 
-import net.minecraft.world.entity.vehicle.EntityMinecartAbstract;
+import net.minecraft.world.entity.vehicle.EntityMinecartRideable;
 import org.bukkit.craftbukkit.CraftServer;
 import org.bukkit.entity.minecart.RideableMinecart;
 
 public class CraftMinecartRideable extends CraftMinecart implements RideableMinecart {
-    public CraftMinecartRideable(CraftServer server, EntityMinecartAbstract entity) {
+    public CraftMinecartRideable(CraftServer server, EntityMinecartRideable entity) {
         super(server, entity);
     }
 
